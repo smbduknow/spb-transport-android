183c2ce1c1b0dfc83f98423b9569b6e6abad94a5
smbduknow <smbduknow@gmail.com>
2017-09-18 16:44:03 +0300

    Fix pin drawing with anti-aliasing

--- app/src/main/java/me/smbduknow/transport/commons/DrawableUtil.kt
+++ app/src/main/java/me/smbduknow/transport/commons/DrawableUtil.kt
@@ -2,32 +2,34 @@ package me.smbduknow.transport.commons
 
 import android.content.Context
 import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
+import android.os.Build
 import android.support.annotation.DrawableRes
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.content.res.AppCompatResources
 import android.text.TextPaint
 import me.smbduknow.transport.R
 
+
 object DrawableUtil {
 
     fun createVehiclePin(ctx: Context, @DrawableRes iconRes: Int, text: String, angle: Float): Bitmap {
-        val drawable = AppCompatResources.getDrawable(ctx, iconRes)
-                ?: throw Resources.NotFoundException("Vehicle icon not found")
 
-        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val iconBitmap = getBitmapFromVectorDrawable(ctx, iconRes)
+
+        val bitmap = Bitmap.createBitmap(iconBitmap.width, iconBitmap.height, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(bitmap)
 
         // draw icon
-        val canvas = Canvas(bitmap)
-        val cWidth = canvas.width
-        val cHeight = canvas.height
-        drawable.setBounds(0, 0, cWidth, cHeight)
-        canvas.rotate(-90 + angle, cWidth / 2f, cHeight /2f)
-        drawable.draw(canvas)
-
-        canvas.rotate(90 -angle, cWidth / 2f, cHeight /2f)
+        val bitmapPaint = Paint().apply {
+            isAntiAlias = true
+            isFilterBitmap = true
+        }
+        val matrix = Matrix().apply{
+            setRotate(-90 + angle, iconBitmap.width / 2f, iconBitmap.height / 2f)
+        }
+        canvas.drawBitmap(iconBitmap, matrix, bitmapPaint)
 
         // draw text
         val fontSize = ctx.resources.getDimensionPixelSize(R.dimen.text_pin).toFloat()
@@ -38,7 +40,23 @@ object DrawableUtil {
             textAlign = Paint.Align.CENTER
         }
 
-        canvas.drawText(text, (bitmap.width / 2).toFloat(), (bitmap.height / 2 + 8).toFloat(), textPaint)
+        canvas.drawText(text, (bitmap.width / 2).toFloat(), (bitmap.height / 2 + fontSize/3), textPaint)
+
+        return bitmap
+    }
+
+    private fun getBitmapFromVectorDrawable(context: Context, @DrawableRes drawableId: Int): Bitmap {
+        var drawable = AppCompatResources.getDrawable(context, drawableId)
+                ?: throw Resources.NotFoundException("Vehicle icon not found")
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            drawable = DrawableCompat.wrap(drawable).mutate()
+        }
+
+        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
 
         return bitmap
     }
